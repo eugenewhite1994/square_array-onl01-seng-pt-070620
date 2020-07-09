d6a9191968168bbb2d4322e535fb33cb2bd7251d
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 22:19:07 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,3 +4,12 @@ def square_array(array)
     puts#[number * 2}
 end
 end
+
+# ef square_array(array)
+#   arr = []
+#   array.each { |i| arr << i ** 2 }
+#   arr
+# end
+
+# my_arr = [1, 2]
+# p square_array(my_arr) #=> [1, 4]
