3c53ab98461521200378beebffc434ecff6345dc
Eugene White <eugenewhite1994@gmail.com>
2020-07-09 22:15:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  numbers = [1,2,5]
+  numbers.each do |number|
+    puts #{number} * #{number}
 end
+square_array([2,4,5])
+
+# primary_colors = ["Red", "Yellow", "Blue"]
+# primary_colors.each do |color|
+#   puts "Primary Color #{color} is #{color.length} letters long."
+# end
